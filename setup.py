69f4bf71ccb7e21032217e5c069e62b412db5a0c
david-schr <david.schrenk@protonmail.com>
2021-10-19 13:42:25 +0200

    Remove requests module

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='studip-news',
     version='1.0',
-    packages=['requests', 'html2text', 'feedgen', 'studip-sync'],
+    packages=['html2text', 'feedgen', 'studip-sync'],
     url='',
     license='',
     author='David Schrenk',
     author_email='david.schrenk@protonmail.com',
-    description='Fetches the activity stream from the StudIp platfrom deployed at the University of Passau and converts it to an atom feed.'
+    description='Fetches the activity stream from the Stud.IP platform deployed at the University of Passau and converts it to an atom feed.'
 )
