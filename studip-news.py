edb8644ef6a469eff4b348fde780f299f2db3467
david-schr <david.schrenk@protonmail.com>
2021-10-19 13:42:46 +0200

    Automatically fetch user id

--- studip-news.py
+++ studip-news.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import re
+
 import html2text
 from config import Config
 from feedgen.feed import FeedGenerator
@@ -9,15 +11,15 @@ from studip_sync.logins import LoginError
 from studip_sync.session import Session, SessionError
 from studip_sync.parsers import ParserError
 
-ACTIVITY_URL = Config.BASE_URL + '/studip/api.php/user/' + Config.USER_ID + '/activitystream'
 PAYLOAD = {'filtertype': Config.SELECTED_FILTERS}
 
 
 def main():
     session = create_session()
+    userid = extract_userid(session)
     print("Fetching stream...")
 
-    activities = fetch_activitystream(session)
+    activities = fetch_activitystream(session, userid)
     generate_feed(activities)
 
 
@@ -26,7 +28,7 @@ def create_session():
         print("Logging in...")
         try:
             studip_sync_session.login(CONFIG.auth_type, CONFIG.auth_type_data, CONFIG.username,
-                          CONFIG.password)
+                                      CONFIG.password)
         except (LoginError, ParserError) as e:
             print("Login failed!")
             print(e)
@@ -35,24 +37,41 @@ def create_session():
     return studip_sync_session.session
 
 
+def extract_userid(session):
+    if Config.USER_ID:
+        return Config.USER_ID
+
+    with session.get(Config.BASE_URL + "/studip/dispatch.php/start") as response:
+        if not response.ok:
+            raise SessionError("Could not open page for userid extraction!")
+
+    match = re.search('STUDIP.ActivityFeed.user_id = \'(.+?)\';', response.text)
+    if match:
+        userid = match.group(1)
+        return userid
+    else:
+        raise ParserError("Could not extract userid!")
+
+
 def generate_feed(json):
     fg = FeedGenerator()
-    fg.id(ACTIVITY_URL)
+    fg.id(Config.BASE_URL)
     fg.title('StudIp Activities')
 
     for entry in json:
         fe = fg.add_entry()
         fe.id(entry['id'])
         fe.title(entry['title'])
-        fe.content(html2text.html2text(entry['content']))
-        fe.link({'href':  Config.BASE_URL + list(entry['object_url'].keys())[0]})
+        fe.content(html2text.html2text(entry['content']), type="CDATA")
+        fe.link({'href': Config.BASE_URL + list(entry['object_url'].keys())[0]})
         fe.published(datetime.fromtimestamp(int(entry['mkdate']), timezone.utc))
 
-    fg.atom_file("/dev/stdout")
+    fg.atom_file("/dev/stdout", pretty=True)
 
 
-def fetch_activitystream(session):
-    with session.get(ACTIVITY_URL, params=PAYLOAD) as response:
+def fetch_activitystream(session, userid):
+    with session.get(Config.BASE_URL + '/studip/api.php/user/' + userid + '/activitystream',
+                     params=PAYLOAD) as response:
         if not response.ok:
             raise SessionError("Could not fetch activities!")
 
